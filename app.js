72a93d1ac19554cd750c00a2a3f0e8043ff1dc93
KordianMikolajczyk <75573262+KordianMikolajczyk@users.noreply.github.com>
2022-02-05 16:56:44 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const config = require('./config');
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Witam w aplikacji zaliczajacej Kordiana Mikolajczyka');
+  res.send('repozytorium dostpne jest pod wskazanym adresem: https://github.com/KordianMikolajczyk/AzureDockerApp.git');
+});
+
+app.listen(config.port, () => {
+  console.log(`Port serwera to: ${config.port}`);
+});
